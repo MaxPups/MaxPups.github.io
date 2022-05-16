430bfa3bfd09f1ea1869d16e1d1fb40c464eccf5
MaxP-G <77100638+MaxPups@users.noreply.github.com>
2022-05-16 13:25:12 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,11 +6,11 @@ $('#calendar-id').bitroidCalendarEv(
         format: "DD.MM.YYYY",
         showEventBlock: true,
         events : [
-            {
+             {
                 day : "03",
                 month : "02",
                 year : "2021",
-                event_description : "Описание события",
+                event_description : `<a href="https://www.youtube.com/"></a>`,
                 event_title : "Заголовок для события",
                 event_body : "Описание для события"
             },
@@ -21,6 +21,33 @@ $('#calendar-id').bitroidCalendarEv(
                 event_description : "Описание события2",
                 event_title : "Заголовок для события2",
                 event_body : "Описание для события2"
+            },
+            {
+                day : "15",
+                month : "02",
+                year : "2022",
+                event_description : "событие САШИ",
+                event_title : "что-то произойдеи",
+                event_body : "Описание для события"
+                // event_body : "Описание для события2"
+            },
+            {
+                day : "15",
+                month : "05",
+                year : "2022",
+                event_description : "событие САШИ",
+                event_title : "что-то произойдеи",
+                event_body : "Описание для события"
+                // event_body : "Описание для события2"
+            },
+            {
+                day : "16",
+                month : "05",
+                year : "2022",
+                event_description : "событие САШИ@@",
+                event_title : "что-то произойдеи?????",
+                event_body : "12312312asdbnfasdbfnainsd"
+                // event_body : "Описание для события2"
             },
     
         ],
